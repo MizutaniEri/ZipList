4a46d5926f155a1c142d0ae17170c1ec669e963e
水谷絵里 <kirishima.mana.eva@gmail.com>
2017-08-23 17:18:03 +0900

    新規追加

---
+++ ZipList/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ZipList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.extractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(528, 453);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "名前";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "サイズ";
+            this.columnHeader2.Width = 90;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "圧縮サイズ";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "最終更新日時";
+            this.columnHeader4.Width = 120;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.addToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(107, 54);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(103, 6);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.extractToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(119, 48);
+            // 
+            // extractToolStripMenuItem
+            // 
+            this.extractToolStripMenuItem.Name = "extractToolStripMenuItem";
+            this.extractToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.extractToolStripMenuItem.Text = "Extract";
+            this.extractToolStripMenuItem.Click += new System.EventHandler(this.extractToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 453);
+            this.Controls.Add(this.listView1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "ZipList";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem extractToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ ZipList/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZipList
+{
+    public partial class Form1 : Form
+    {
+        private string zipFile = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.Filter = "ZIP File|*.zip";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                zipFile = openDialog.FileName;
+                zipList(zipFile);
+                Text = Path.GetFileName(zipFile);
+            }
+        }
+
+        private void zipList(string fileName)
+        {
+            listView1.Items.Clear();
+            using (var zipArc = ZipFile.OpenRead(fileName))
+            {
+                zipArc.Entries.ToList().ForEach(entry =>
+                {
+                    var item = new ListViewItem(new string[]
+                    {
+                        entry.FullName,
+                        entry.Length.ToString("#,0"),
+                        entry.CompressedLength.ToString("#,0"),
+                        entry.LastWriteTime.ToString("yyyy/MM/dd HH:mm:ss")
+                    });
+                    listView1.Items.Add(item);
+                });
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    contextMenuStrip2.Show(listView1, e.Location);
+                }
+                else
+                {
+                    contextMenuStrip1.Show(listView1, e.Location);
+                }
+            }
+        }
+
+        private void extractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count <= 0)
+            {
+                return;
+            }
+            var saveDialog = new SaveFileDialog();
+            if (saveDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            var list = new List<ListViewItem>();
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                list.Add(item);
+            }
+            var saveFolder = Path.GetDirectoryName(saveDialog.FileName);
+            using (var zipArc = ZipFile.OpenRead(zipFile))
+            {
+                list.ForEach(item =>
+                {
+                    var entry = zipArc.GetEntry(item.SubItems[0].Text);
+                    entry.ExtractToFile(Path.Combine(saveFolder, entry.Name), true);
+                });
+            }
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (zipFile == string.Empty)
+            {
+                newZipFileCreateAdd();
+            }
+            zipFileAdd();
+        }
+
+        private async void zipFileAdd()
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.Multiselect = true;
+            openDialog.Filter = "All File|*.*";
+            openDialog.Title = "追加するファイル指定";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                await Task.Run(() =>
+                {
+                    // オンメモリで操作する関係上、ファイル単位でOpen～書き込みを行う
+                    openDialog.FileNames.ToList().ForEach(file =>
+                    {
+                        //読み取りと書き込みができるようにして、ZIP書庫を開く
+                        using (ZipArchive a = ZipFile.Open(zipFile, ZipArchiveMode.Update, Encoding.GetEncoding("shift_jis")))
+                        {
+                            //ファイル「C:\test\1.txt」を「1.txt」としてZIPに追加する
+                            var e = a.CreateEntryFromFile(file, Path.GetFileName(file));
+                            var fl = new FileInfo(file);
+                            var item = new ListViewItem(new string[]
+                            {
+                                e.FullName,
+                                fl.Length.ToString("#,0"),
+                                0.ToString("#,0"),
+                                e.LastWriteTime.ToString("yyyy/MM/dd HH:mm:ss")
+                            });
+                            
+                            this.Invoke((MethodInvoker)(() => listView1.Items.Add(item)));
+                        }
+                    });
+                });
+            }
+        }
+
+        private void newZipFileCreateAdd()
+        {
+            var saveZipDialog = new SaveFileDialog();
+            saveZipDialog.Filter = "ZIP File|*.zip";
+            if (saveZipDialog.ShowDialog() == DialogResult.OK)
+            {
+                zipFile = saveZipDialog.FileName;
+                Text = Path.GetFileName(zipFile);
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var list = new List<ListViewItem>();
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                list.Add(item);
+            }
+            using (var zipArc = ZipFile.Open(zipFile, ZipArchiveMode.Update))
+            {
+                list.ForEach(item =>
+                {
+                    var entry = zipArc.GetEntry(item.Text);
+                    entry.Delete();
+                    listView1.Items.Remove(item);
+                });
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            var item = listView1.SelectedItems[0];
+            Process.Start(item.Text);
+        }
+    }
+}
